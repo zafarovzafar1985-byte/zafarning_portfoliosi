2d3deaca58d754a88adc1f8ef7faee6ff7a8fde2
Zafar <zafarovzafar1985@gmail.com>
2026-05-22 16:56:31 +0500

    Portfolio

---
+++ script.js
@@ -0,0 +1,8 @@
+function Batafsil() {
+    var surovnoma = prompt("Men bilan bog'lanmoqchimisiz? (Ha yoki Yo'q)");
+    if (surovnoma == "Ha") {
+        alert(" Men 13 yoshdaman,Men Surxandaryoda yashayman,Mening nomerim: +998 418 36 34")
+    }else {
+        alert("Yaxshi,Korishguncha!")
+    }
+}
